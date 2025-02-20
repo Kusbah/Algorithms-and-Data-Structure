ba6148ed31663e2c01ed7b42a194d789c2d24a7a
kusbah <abdkusbah2@gmail.com>
2025-02-20 12:47:14 +0200

    complete Practic |

---
+++ Practice I/script.js
@@ -0,0 +1,26 @@
+var favoriteAnimal = "cat";
+console.log(favoriteAnimal);
+
+var a = 5;
+var b = 3;
+console.log(sum=a+b);
+
+var age = 23;
+console.log("I am "+age+" years old");
+
+var country = "United Kingdom";
+country = "palestine";
+console.log(country);
+
+const pi = 3.14;
+console.log(pi);
+
+var favoriteColor = "blue";
+console.log(favoriteColor);
+
+var x = 5;
+var y = 10;
+console.log(sum =  x + y);
+
+var isSunny = true;
+console.log("It is sunny today "+ isSunny)
