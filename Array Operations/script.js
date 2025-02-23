34beaff42ec152e8a3ed2345ffc3c0d509c3c269
kusbah <abdkusbah2@gmail.com>
2025-02-23 14:16:24 +0200

    complete Array Operations

--- Array Operations/script.js
+++ Array Operations/script.js
@@ -17,3 +17,59 @@ if(test)
     console.log("Found at position: "+numbers2.indexOf(25))
 else
     console.log("Not Found")
+//4. Sorting an Array
+
+let scores = [50, 20, 70, 10, 40];
+scores.sort();
+console.log(scores)
+scores.sort((a,b)=> b-a);
+console.log(scores)
+let names = ["Shatha", "Sara", "Lina", "Sami", "Dalia"];
+names.sort();
+console.log(names)
+//5. Inserting Elements
+let animals = ["dog", "cat", "rabbit"];
+animals.push("elephant");
+animals.unshift("lion")
+animals.splice(2,0,"tiger")
+console.log(animals)
+//6. Deleting Elements
+let fruits = ["apple", "banana", "cherry", "date"];
+fruits.pop();
+fruits.shift();
+fruits.splice(0,1)
+console.log(fruits)
+//7. Combining Arrays
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let array3 = array1.concat(array2);
+console.log(array3);
+//8. Splitting an Array
+let items = ["a", "b", "c", "d", "e"];
+let items2 = items.splice(0,3)
+console.log(items2+ " " +items)
+//9. Filtering Elements
+let numberss = [1, 5, 10, 15, 20, 25, 30];
+let  arrayy = []
+for(var i = 0; i<numberss.length;i++){
+    if(numberss[i]>15){
+        arrayy.push(numberss[i])
+    }
+}
+console.log(arrayy)
+//10. Advanced Challenge
+var array10 = [1,2,2,3,4,4,5];
+var result = [];
+for(var i = 0 ; i < array10.length;i++){
+    if(!result.includes(array10[i])){
+        result.push(array10[i])
+    }
+}
+console.log(result)
+//b
+var arrayb = [1,2,3,4,5];
+var n = 2
+let part1 = arrayb.slice(-n);
+let part2 = arrayb.slice(0, -n);
+
+console.log(part1.concat(part2)); 
